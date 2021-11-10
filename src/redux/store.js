50e3f072dee2794d2fb45cebc15d27324dfcb0f0
Alexander Dimitryuk <dimitryuk38@gmail.com>
2021-11-10 15:50:08 +0200

    start

---
+++ src/redux/store.js
@@ -0,0 +1,4 @@
+import { createStore } from "redux";
+import { contactReducer } from "./contacts/reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+export const store = createStore(contactReducer, composeWithDevTools());
