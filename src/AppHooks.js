ae337abb83319030427f068f1fb41c4be21199d7
Dimitryuk <79190561+Dimitryuk@users.noreply.github.com>
2021-11-21 19:55:55 +0200

    start

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -1,21 +1,21 @@
+import './App.css';
+import AppBar from './AppBar';
+import ContactForm from './ContactForm/ContactFormHooks';
+import ContactList from './ContactList/ContactListHooks';
 
-
-import "./App.css";
-import ContactForm from "./ContactForm/ContactFormHooks";
-import ContactList from "./ContactList/ContactListHooks";
-
-import Filter from "./Filter/FilterHooks";
+import Filter from './Filter/FilterHooks';
 
 function App() {
   return (
     <>
       <div className="App">
-         <h1>Phonebook</h1>
-         <ContactForm  />
-         <h2>Contacts</h2>
-        <Filter  />
-         <ContactList/>
-       </div>
+        <h1>Phonebook</h1>
+        <AppBar />
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </div>
     </>
   );
 }
